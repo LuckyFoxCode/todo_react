acaccfd5a2bd08a0726ea7bd8c4dbe60ad2b6eeb
Serhii Shabala <sergei181989@gmail.com>
2024-01-04 22:59:04 +0200

    Feat: Add note in global data

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,4 @@
+import { useState } from 'react';
 import { Card } from '@/components/ui/card';
 import { TodoList } from '@/components/TodoList';
 import { TodoForm } from './components/TodoForm';
@@ -8,17 +9,17 @@ export interface TodosProps {
   isDone: boolean;
 }
 
-const todos: TodosProps[] = [
-  { id: '0', note: 'Awesome library Shadcn/UI 🔥', isDone: true },
-  { id: '1', note: 'React is it freamwork or library? 🙃', isDone: false },
-  { id: '3', note: 'Added new task in Super Todo App 👀', isDone: true },
-];
-
 export const App = () => {
+  const [todos, setTodos] = useState<TodosProps[]>([
+    { id: '0', note: 'Awesome library Shadcn/UI 🔥', isDone: true },
+    { id: '1', note: 'React is it freamwork or library? 🙃', isDone: false },
+    { id: '3', note: 'Added new task in Super Todo App 👀', isDone: true },
+  ]);
+
   return (
     <Card className="w-3/4 bg-slate-600 border-none m-auto mt-5 p-4">
       <h1 className="text-center text-cyan-400 mb-3">Super Todo:</h1>
-      <TodoForm />
+      <TodoForm setTodos={setTodos} todos={todos} />
       <TodoList todos={todos} />
     </Card>
   );
--- src/components/TodoForm.tsx
+++ src/components/TodoForm.tsx
@@ -1,17 +1,69 @@
+import { useForm } from 'react-hook-form';
+import * as zod from 'zod';
+import { TodosProps } from '@/App';
+import { zodResolver } from '@hookform/resolvers/zod';
 import { Button } from './ui/button';
-import { Card } from './ui/card';
+import { Form, FormControl, FormField, FormItem, FormMessage } from './ui/form';
 import { Input } from './ui/input';
 
-export const TodoForm = () => {
+interface TodoFormProps {
+  todos: TodosProps;
+  setTodos: React.Dispatch<React.SetStateAction<TodosProps[]>>;
+}
+
+const formSchema = zod.object({
+  note: zod.string().min(2, {
+    message: 'Note must be at least 2 characters',
+  }),
+});
+
+export const TodoForm: React.FC<TodoFormProps> = ({ setTodos }) => {
+  const form = useForm<zod.infer<typeof formSchema>>({
+    resolver: zodResolver(formSchema),
+    defaultValues: {
+      note: '',
+    },
+  });
+
+  const handleOnSubmit = (values: zod.infer<typeof formSchema>) => {
+    setTodos((prevState) => [
+      ...prevState,
+      { id: crypto.randomUUID(), note: values.note, isDone: true },
+    ]);
+
+    values.note = '';
+  };
+
   return (
-    <Card className="bg-transparent border-none flex">
-      <Input
-        placeholder="Edit..."
-        className=" focus-visible:ring-cyan-400 mr-2"
-      />
-      <Button variant={'outline'} className="focus-visible:ring-cyan-400">
-        Add task
-      </Button>
-    </Card>
+    <Form {...form}>
+      <form
+        onSubmit={form.handleSubmit(handleOnSubmit)}
+        className=" bg-transparent border-none flex"
+      >
+        <FormField
+          control={form.control}
+          name="note"
+          render={({ field }) => (
+            <FormItem className="w-full">
+              <FormControl>
+                <Input
+                  placeholder="Edit notes..."
+                  {...field}
+                  className="w-full focus-visible:ring-cyan-400 mr-2"
+                />
+              </FormControl>
+              <FormMessage />
+            </FormItem>
+          )}
+        />
+        <Button
+          type="submit"
+          variant={'outline'}
+          className="focus-visible:ring-cyan-400 ml-2"
+        >
+          Add note
+        </Button>
+      </form>
+    </Form>
   );
 };
