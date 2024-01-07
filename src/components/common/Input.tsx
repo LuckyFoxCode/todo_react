21207f6a5dee2d8f10a117d9c80b58ea28bb9ee9
Serhii Shabala <sergei181989@gmail.com>
2024-01-07 13:56:27 +0200

    :recycle: Refactor Input component.

--- src/components/common/Input.tsx
+++ src/components/common/Input.tsx
@@ -1,26 +1,29 @@
 import clsx from 'clsx';
 
 interface InputProps {
-  type: 'text' | 'password';
-  placeholder: string;
-  className?: string;
+  name?: string;
   value?: string;
+  className?: string;
+  placeholder: string;
+  type: 'text' | 'password' | 'email';
   onChange?: (e: React.ChangeEvent<HTMLInputElement>) => void;
 }
 
 export const Input: React.FC<InputProps> = ({
+  name,
   type,
-  placeholder,
+  value,
   onChange,
   className,
-  value,
+  placeholder,
 }) => {
   return (
     <input
+      name={name}
       type={type}
-      placeholder={placeholder}
-      onChange={onChange}
       value={value}
+      onChange={onChange}
+      placeholder={placeholder}
       className={clsx(
         'grow bg-slate-100 px-3 py-2 rounded-lg focus:outline-cyan-500 text-cyan-500',
         className,
