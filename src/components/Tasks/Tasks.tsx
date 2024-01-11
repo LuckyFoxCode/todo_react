712e45e9651399c1727ea082f50462119313d346
Serhii Shabala <sergei181989@gmail.com>
2024-01-11 23:34:42 +0200

    :recycle: Refactor: render empty page without selected category.

--- src/components/Tasks/Tasks.tsx
+++ src/components/Tasks/Tasks.tsx
@@ -1,4 +1,3 @@
-import { tasks } from 'data/tasks';
 import { SelectedCategory } from 'utils/types';
 import { TasksList } from './components';
 import s from './Tasks.module.scss';
@@ -10,7 +9,7 @@ interface TasksProps {
 export const Tasks: React.FC<TasksProps> = ({ selectedCategory }) => {
   return (
     <div className={s.tasks}>
-      {tasks.length === 0 ? (
+      {selectedCategory.title.length === 0 ? (
         <span className={s.tasks__descr}>Tasks is empty... 😢</span>
       ) : (
         <TasksList selectedCategory={selectedCategory} />
